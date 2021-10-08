1e3c10dcecf8e8a39aad5f5f910bc2805e9f6063
CJ42 <cavallerajean@gmail.com>
2021-10-08 07:36:25 +0100

    test: add function to YWriter

--- implementations/contracts/helpers/ERC725YWriter.sol
+++ implementations/contracts/helpers/ERC725YWriter.sol
@@ -19,9 +19,21 @@ contract ERC725YWriter {
         
         // create the value
         bytes[] memory _values = new bytes[](1);
-        _values[0] = abi.encode("Hello Lukso!");
+        _values[0] = abi.encodePacked("Hello Lukso!");
 
         IERC725Y(to).setData(_keys, _values);
+    }
+
+    function setFiveComputedData(address to, uint256 amountOfEntries) public {
+        bytes32[] memory keys = new bytes32[](amountOfEntries);
+        bytes[] memory values = new bytes[](amountOfEntries);
+
+        for (uint256 i=0; i < amountOfEntries; i++) {
+            keys[i] = abi.decode(abi.encodePacked("key", uint232(i)), (bytes32));
+            values[i] = abi.encodePacked("value", uint8(i));
+        }
+
+        IERC725Y(to).setData(keys, values);
     }
 
 }
